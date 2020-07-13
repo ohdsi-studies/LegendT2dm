06b8cf7488f96a3e2651c365422bfb612734f5ee
Marc Suchard <msuchard@ucla.edu>
2020-07-13 11:56:08 -0700

    initial class level cohorts

--- extra/Untitled.R
+++ extra/Untitled.R
@@ -1,6 +1,12 @@
 # Make ExposuresOfInterest.csv
 library(dplyr)
 
+
+baseUrlPublic <- baseUrl = "http://atlas-demo.ohdsi.org:80/WebAPI"
+baseUrlJnj <- baseUrl = "http://atlas-demo.ohdsi.org:80/WebAPI"
+
+
+function() {
 exp <- readr::read_csv("inst/settings/ExposuresOfInterest.csv") %>% group_by(class) %>% arrange(class,name)
 
 dpp4isConceptIds <- exp %>% filter(class == "DPP4Is") %>% arrange(cohortId) %>% pull(cohortId) %>% paste0(collapse = ";")
@@ -15,4 +21,46 @@ exp[!is.na(exp$shortName) &exp$shortName == "SUs", "includedConceptIds"] <- susC
 
 readr::write_csv(exp, "inst/settings/tmp.csv")
 
-readr::read_csv("inst/settings/ExcludedIngredientConcepts.csv") %>% pull(cohortId) %>% paste0(collapse = ";")
+readr::read_csv("inst/settings/ExcludedIngredientConcepts.csv") %>% arrange(conceptId) %>% pull(conceptId) %>% paste0(collapse = ";")
+
+}
+
+baseCohort <- ROhdsiWebApi::getCohortDefinition(1774646, baseUrl = baseUrlPublic)
+#saveRDS(baseCohort, file = "baseCohort.rds")
+
+permutations <- readr::read_csv("classComparisons.csv")
+exposuresOfInterest <- readr::read_csv("inst/settings/ExposuresOfInterest.csv") %>% select(cohortId, shortName)
+permutations <- inner_join(permutations, exposuresOfInterest, by = c("targetId" = "cohortId"))
+
+permuteTC <- function(cohort,
+                      permutation) {
+  cohort$expression$PrimaryCriteria$CriteriaList[[1]]$DrugExposure$CodesetId <- permutation$targetId
+  cohort$expression$AdditionalCriteria$CriteriaList[[1]]$Criteria$DrugExposure$CodesetId <- permutation$comparator1Id
+  cohort$expression$AdditionalCriteria$CriteriaList[[2]]$Criteria$DrugExposure$CodesetId <- permutation$comparator2Id
+  cohort$expression$AdditionalCriteria$CriteriaList[[3]]$Criteria$DrugExposure$CodesetId <- permutation$comparator3Id
+
+  if (permutation$cvd == "high") {
+    # Do nothing
+  } else if (permutation$cvd == "low") {
+    cohort$expression$InclusionRules[[2]]$name <- "Low cardiovascular risk"
+    cohort$expression$InclusionRules[[2]]$description <- "Identify patients at low cardiovascular risk (based on Ryan et al. 2018 Diabetes Obes Metab)"
+    cohort$expression$InclusionRules[[2]]$expression$Type <- "ALL"
+
+    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[1]]$Occurrence$Type <- 0
+    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[1]]$Occurrence$Count <- 0
+
+    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[2]]$Occurrence$Type <- 0
+    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[2]]$Occurrence$Count <- 0
+  } else if (permutation$cvd == "all") {
+    cohort$expression$InclusionRules[[2]] <- NULL
+  } else {
+    stop("Unknown CVD risk type")
+  }
+
+  cohort$name <- paste0(cohort$name, " T: ", permutation$shortName, " CVD: ", permutation$cvd)
+  return(cohort)
+}
+
+c1 <- permuteTC(baseCohort, permutations[12,])
+ROhdsiWebApi::postCohortDefinition(c1$name, c1, baseUrl = baseUrlJnj)
+
