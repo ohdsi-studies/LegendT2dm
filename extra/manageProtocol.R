0c558ba29bfa5e1ca469cb7c8f04b97cf25f1780
Marc Suchard <msuchard@ucla.edu>
2020-09-24 10:15:28 -0700

    working with rmdrive

---
+++ extra/manageProtocol.R
@@ -0,0 +1,7 @@
+
+# Upload Rmd to GDocs (note: will overwrite current document)
+rmdrive::upload_rmd(file = "Documents/Protocol",
+                    gfile = "LEGEND-T2DM_Protocol",
+                    path = "LEGEND_grant_proposal_documents")
+
+
