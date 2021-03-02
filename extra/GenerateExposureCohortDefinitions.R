c7729421d62f89cc5b5cd449fbf2330289659a9f
Marc Suchard <msuchard@ucla.edu>
2021-03-02 14:48:36 -0800

    updating class-by-class jsons

--- extra/GenerateExposureCohortDefinitions.R
+++ extra/GenerateExposureCohortDefinitions.R
@@ -2,9 +2,8 @@
 library(dplyr)
 
 
-baseUrlPublic <- keyring::key_get("ohdsiBaseUrl")
-baseUrlJnj <- keyring::key_get("baseUrl")
-
+#baseUrlPublic <- keyring::key_get("ohdsiBaseUrl")
+#baseUrlJnj <- keyring::key_get("baseUrl")
 
 function() {
   exp <- readr::read_csv("inst/settings/ExposuresOfInterest.csv") %>% group_by(class) %>% arrange(class,name)
@@ -25,100 +24,397 @@ function() {
 
 }
 
+function() {
+
+  idFn <- function(targetId, age, sex, race, cvd, renal, tar, met) {
+    cAge <- -1
+    if (age == "all") {
+      cAge <- 0
+    } else if (age == "younger") {
+      cAge <- 1
+    } else if (age == "middle") {
+      cAge <- 2
+    } else if (age == "older") {
+      cAge <- 3
+    } else {
+      stop("Unknown age")
+    }
+
+    cSex <- -1
+    if (sex == "all") {
+      cSex <- 0
+    } else if (sex == "female") {
+      cSex <- 1
+    } else if (sex == "male") {
+      cSex <- 2
+    } else {
+      stop("Unknown sex")
+    }
+
+    cRace <- -1
+    if (race == "all") {
+      cRace <- 0
+    } else if (race == "black") {
+      cRace <- 1
+    } else {
+      stop("Unknown race")
+    }
+
+    cCvd <- -1
+    if (cvd == "all") {
+      cCvd <- 0
+    } else if (cvd == "low") {
+      cCvd <- 1
+    } else if (cvd == "higher") {
+      cCvd <- 2
+    } else {
+      stop("Unknown cvd")
+    }
+
+    cRenal <- -1
+    if (renal == "all") {
+      cRenal <- 0
+    } else if (renal == "without") {
+      cRenal <- 1
+    } else if (renal == "with") {
+      cRenal <- 2
+    } else {
+      stop("Unknown renal")
+    }
+
+    cTar <- -1
+    if (tar == "ot1") {
+      cTar <- 1
+    } else if (tar == "ot2") {
+      cTar <- 2
+    } else {
+      stop("Unknown met")
+    }
+
+    cMet <- -1
+    if (met == "prior") {
+      cMet <- 1
+    } else if (met == "none") {
+      cMet <- 2
+    } else {
+      stop("Unknown met")
+    }
+
+    result <- paste0(targetId, cTar, cMet, cAge, cSex, cRace, cCvd, cRenal, collapse = "")
+
+    return(result)
+  }
+
+  #tab <- readr::read_csv("inst/settings/classComparisonsOld.csv")
+  # tab <- tab %>% mutate(met = "prior")
+  # tab2 <- tab %>% mutate(met = "none")
+  # tab <- rbind(tab, tab2)
+  #readr::write_csv(tab, "inst/settings/classComparisons.csv")
+
+  tab <- tab %>%
+   # mutate(targetId = targetId * 100,
+   #       comparator1Id = comparator1Id * 100,
+   #       comparator2Id = comparator2Id * 100,
+   #       comparator3Id = comparator3Id * 100) %>%
+    rowwise() %>%  mutate(cohortId = idFn(targetId, age, sex, race, cvd, renal, tar, met))
+}
+
 # OHDSI's public webAPI is down right now:
 # baseCohort <- ROhdsiWebApi::getCohortDefinition(1774646, baseUrl = baseUrlPublic)
-baseCohort <- ROhdsiWebApi::getCohortDefinition(17111, baseUrl = baseUrlJnj)
+# baseCohort <- ROhdsiWebApi::getCohortDefinition(17111, baseUrl = baseUrlJnj)
 
+# baseCohort <- ROhdsiWebApi::getCohortDefinition(1487, baseUrl = "http://atlas-covid19.ohdsi.org/WebAPI")
+# baseCohortJson <- RJSONIO::toJSON(baseCohort$expression, digits = 50)
+# SqlRender::writeSql(baseCohortJson, targetFile = "baseCohort.json")
+# saveRDS(baseCohort, file = "baseCohort.rds")
 
-#saveRDS(baseCohort, file = "baseCohort.rds")
+# Inclusion rules: Age == 1, Sex == 2, Race == 3, CVD == 4, Renal == 5
+
+baseCohort <- readRDS("baseCohort.rds")
 
 generateStats <- TRUE
 
 permutations <- readr::read_csv("inst/settings/classComparisons.csv")
-exposuresOfInterest <- readr::read_csv("inst/settings/ExposuresOfInterest.csv") %>% select(cohortId, shortName)
-permutations <- inner_join(permutations, exposuresOfInterest, by = c("targetId" = "cohortId"))
+exposuresOfInterestTable <- readr::read_csv("inst/settings/ExposuresOfInterest.csv")
+permutations <- inner_join(permutations, exposuresOfInterestTable %>% select(cohortId, shortName), by = c("targetId" = "cohortId"))
 
-permuteTC <- function(cohort,
-                      permutation) {
-  cohort$expression$PrimaryCriteria$CriteriaList[[1]]$DrugExposure$CodesetId <- permutation$targetId
-  cohort$expression$AdditionalCriteria$CriteriaList[[1]]$Criteria$DrugExposure$CodesetId <- permutation$comparator1Id
-  cohort$expression$AdditionalCriteria$CriteriaList[[2]]$Criteria$DrugExposure$CodesetId <- permutation$comparator2Id
-  cohort$expression$AdditionalCriteria$CriteriaList[[3]]$Criteria$DrugExposure$CodesetId <- permutation$comparator3Id
+permuteTC <- function(cohort, permutation, ingredientLevel = FALSE) {
 
-  if (permutation$cvd == "high") {
-    # Do nothing
-  } else if (permutation$cvd == "low") {
-    cohort$expression$InclusionRules[[2]]$name <- "Low cardiovascular risk"
-    cohort$expression$InclusionRules[[2]]$description <- "Identify patients at low cardiovascular risk (based on Ryan et al. 2018 Diabetes Obes Metab)"
-    cohort$expression$InclusionRules[[2]]$expression$Type <- "ALL"
+  c1Id <- floor(permutation$comparator1Id / 10)
+  c2Id <- floor(permutation$comparator2Id / 10)
+  c3Id <- floor(permutation$comparator3Id / 10)
 
-    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[1]]$Occurrence$Type <- 0
-    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[1]]$Occurrence$Count <- 0
+  # Remove unused alternative within-class
+  if (ingredientLevel) {
 
-    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[2]]$Occurrence$Type <- 0
-    cohort$expression$InclusionRules[[2]]$expression$CriteriaList[[2]]$Occurrence$Count <- 0
+    targetId <- permutation$targetId
+    classId <- floor(targetId / 10)
+
+    classSet <- cohort$expression$ConceptSets[[classId]]
+    targetSet <- classSet
+    excludeSet <- classSet
+
+    drugInfo <- exposuresOfInterestTable %>% filter(cohortId == targetId)
+    name <- drugInfo %>% pull(name)
+    conceptId <- drugInfo %>% pull(conceptId)
+
+    targetSet$name <- name
+    excludeSet$name <- paste(excludeSet$name, "excluding", name)
+    excludeSet$id <- 15
+
+    targetSet$expression$items <- plyr::compact(
+      lapply(targetSet$expression$items, function(item) {
+        if (item$concept$CONCEPT_ID == conceptId) {
+          item
+        } else {
+          NULL
+        }
+      }))
+
+    excludeSet$expression$items <- plyr::compact(
+      lapply(excludeSet$expression$items, function(item) {
+        if (item$concept$CONCEPT_ID != conceptId) {
+          item
+        } else {
+          NULL
+        }
+      }))
+
+    cohort$expression$ConceptSets[[classId]] <- targetSet
+    cohort$expression$ConceptSets[[15]] <- excludeSet
+
+    tId <- classId
+
+  } else {
+    cohort$expression$AdditionalCriteria$CriteriaList[[8]] <- NULL
+    cohort$expression$ConceptSets[[15]] <- NULL
+    tId <- floor(permutation$targetId / 10)
+  }
+
+
+  cohort$expression$PrimaryCriteria$CriteriaList[[1]]$DrugExposure$CodesetId <- tId
+  cohort$expression$AdditionalCriteria$CriteriaList[[1]]$Criteria$DrugExposure$CodesetId <- c1Id
+  cohort$expression$AdditionalCriteria$CriteriaList[[2]]$Criteria$DrugExposure$CodesetId <- c2Id
+  cohort$expression$AdditionalCriteria$CriteriaList[[3]]$Criteria$DrugExposure$CodesetId <- c3Id
+
+
+  cohort$expression$EndStrategy$CustomEra[1] <- tId
+
+  delta <- 0
+
+  age <- 1 - delta
+  if (permutation$age == "younger") {
+    cohort$expression$InclusionRules[[age]]$name <- "Lower age group"
+    cohort$expression$InclusionRules[[age]]$description <- NULL
+    cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[1]]$Age$Op <- "lt"
+    cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[2]] <- NULL
+  } else if (permutation$age == "middle") {
+    cohort$expression$InclusionRules[[age]]$name <- "Middle age group"
+    cohort$expression$InclusionRules[[age]]$description <- NULL
+    cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[1]]$Age$Op <- "gte"
+    # cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[2]]$Age$Op <- ""
+  } else if (permutation$age == "older") {
+    cohort$expression$InclusionRules[[age]]$name <- "Older age group"
+    cohort$expression$InclusionRules[[age]]$description <- NULL
+    cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[2]]$Age$Op <- "gte"
+    cohort$expression$InclusionRules[[age]]$expression$DemographicCriteriaList[[1]] <- NULL
+  } else if (permutation$age == "all") {
+    cohort$expression$InclusionRules[[age]] <- NULL
+    delta <- delta + 1
+  } else {
+    stop("Unknown age type")
+  }
+
+  sex <- 2 - delta
+  if (permutation$sex == "female") {
+    cohort$expression$InclusionRules[[sex]]$name <- "Female stratum"
+    cohort$expression$InclusionRules[[sex]]$description <- NULL
+    cohort$expression$InclusionRules[[sex]]$expression$DemographicCriteriaList[[1]]$Gender[[1]]$CONCEPT_ID <- 8532
+    cohort$expression$InclusionRules[[sex]]$expression$DemographicCriteriaList[[1]]$Gender[[1]]$CONCEPT_NAME <- "female"
+  } else if (permutation$sex == "male") {
+    cohort$expression$InclusionRules[[sex]]$name <- "Male stratum"
+    cohort$expression$InclusionRules[[sex]]$description <- NULL
+    cohort$expression$InclusionRules[[sex]]$expression$DemographicCriteriaList[[1]]$Gender[[1]]$CONCEPT_ID <- 8507
+    cohort$expression$InclusionRules[[sex]]$expression$DemographicCriteriaList[[1]]$Gender[[1]]$CONCEPT_NAME <- "male"
+  } else if (permutation$sex == "all") {
+    cohort$expression$InclusionRules[[sex]] <- NULL
+    delta <- delta + 1
+  } else {
+    stop("Unknown sex type")
+  }
+
+  race <- 3 - delta
+  if (permutation$race == "black") {
+    cohort$expression$InclusionRules[[race]]$name <- "Race stratum"
+    cohort$expression$InclusionRules[[race]]$description <- NULL
+  } else if (permutation$race == "all") {
+    cohort$expression$InclusionRules[[race]] <- NULL
+    delta <- delta + 1
+  } else {
+    stop("Unknown race type")
+  }
+
+  cvd <- 4 - delta
+  if (permutation$cvd == "low") {
+    cohort$expression$InclusionRules[[cvd]]$name <- "Low cardiovascular risk"
+    cohort$expression$InclusionRules[[cvd]]$description <- NULL
+    cohort$expression$InclusionRules[[cvd]]$expression$Type <- "ALL"
+
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[1]]$Occurrence$Type <- 0
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[1]]$Occurrence$Count <- 0
+
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[2]]$Occurrence$Type <- 0
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[2]]$Occurrence$Count <- 0
+  } else if (permutation$cvd == "higher") {
+    cohort$expression$InclusionRules[[cvd]]$name <- "Higher cardiovascular risk"
+    cohort$expression$InclusionRules[[cvd]]$description <- NULL
+    cohort$expression$InclusionRules[[cvd]]$expression$Type <- "ANY"
+
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[1]]$Occurrence$Type <- 2
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[1]]$Occurrence$Count <- 1
+
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[2]]$Occurrence$Type <- 2
+    cohort$expression$InclusionRules[[cvd]]$expression$CriteriaList[[2]]$Occurrence$Count <- 1
   } else if (permutation$cvd == "all") {
-    cohort$expression$InclusionRules[[2]] <- NULL
+    cohort$expression$InclusionRules[[cvd]] <- NULL
+    delta <- delta  + 1
   } else {
     stop("Unknown CVD risk type")
   }
 
-  if (permutation$age == "older") {
-    # Do nothing
-  } else if (permutation$age == "younger") {
-    cohort$expression$InclusionRules[[1]]$expression$DemographicCriteriaList[[1]]$Age$Op <- "lt"
-  } else if (permutation$age == "all") {
-    cohort$expression$InclusionRules[[1]] <- NULL
+  renal <- 5 - delta
+  if (permutation$renal == "without") {
+    cohort$expression$InclusionRules[[renal]]$name <- "Without renal impairment"
+    cohort$expression$InclusionRules[[renal]]$description <- NULL
+    cohort$expression$InclusionRules[[renal]]$expression$CriteriaList[[1]]$Occurrence$Type <- 0
+    cohort$expression$InclusionRules[[renal]]$expression$CriteriaList[[1]]$Occurrence$Count <- 0
+  } else if (permutation$renal == "with") {
+    cohort$expression$InclusionRules[[renal]]$name <- "Renal impairment"
+    cohort$expression$InclusionRules[[renal]]$description <- NULL
+  } else if (permutation$renal == "all") {
+    cohort$expression$InclusionRules[[renal]] <- NULL
+    delta <- delta  + 1
   } else {
-    stop("Unknown age type")
+    stop("Unknown renal type")
+  }
+
+  met <- 6 - delta
+  if (permutation$met == "prior") {
+    # Do nothing
+    cohort$expression$InclusionRules[[met]]$description <- NULL
+  } else if (permutation$met == "none") {
+    # TODO Should change to no prior met instead of no restrictions
+    cohort$expression$InclusionRules[[met]] <- NULL
+    delta <- delta + 1
+  } else {
+    stop("Unknown metformin type")
+  }
+
+  if (permutation$tar == "ot1") {
+    cohort$expression$CensoringCriteria <- list()
+  } else if (permutation$tar == "ot2") {
+
+    includedConcepts <- as.numeric(unlist(strsplit(exposuresOfInterestTable %>%
+                                                     filter(cohortId == permutation$targetId) %>%
+                                                     pull(includedConceptIds),
+                                                   ";")))
+    items <- cohort$expression$ConceptSets[[14]]$expression$items
+
+    tmp <-
+      lapply(items, function(item) {
+        if (item$concept$CONCEPT_ID %in% includedConcepts) {
+          NULL
+        } else {
+          item
+        }
+      })
+    cohort$expression$ConceptSets[[14]]$expression$items <- plyr::compact(tmp)
+  } else {
+    stop("Unknown TAR")
   }
 
   cohort$name <- paste0(cohort$name, " T: ", permutation$shortName, " CVD: ", permutation$cvd, " Age: ", permutation$age)
   return(cohort)
 }
 
-allCohortsSql <-
-  do.call("rbind",
-          lapply(1:nrow(permutations), function(i) {
-            cohortDefinition <- permuteTC(baseCohort, permutations[i,])
-            cohortSql <- ROhdsiWebApi::getCohortSql(cohortDefinition,
-                                                    baseUrlJnj,
-                                                    generateStats = generateStats)
-            return(cohortSql)
-          }))
+# allCohortsSql <-
+#   do.call("rbind",
+#           lapply(1:nrow(permutations), function(i) {
+#             cohortDefinition <- permuteTC(baseCohort, permutations[i,])
+#             cohortSql <- ROhdsiWebApi::getCohortSql(cohortDefinition,
+#                                                     baseUrlJnj,
+#                                                     generateStats = generateStats)
+#             return(cohortSql)
+#           }))
 
 allCohortsJson <-
   do.call("rbind",
           lapply(1:nrow(permutations), function(i) {
             cohortDefinition <- permuteTC(baseCohort, permutations[i,])
-            cohortJson <- RJSONIO::toJSON(cohortDefinition$expression)
+            cohortJson <- RJSONIO::toJSON(cohortDefinition$expression, indent = 2, digits = 10)
             return(cohortJson)
             #return(cohortDefinition)
           }))
 
 
 permutations$json <- allCohortsJson
-readr::write_csv(permutations, path = "inst/settings/classComparisonsWithJson.csv")
+#readr::write_csv(permutations, path = "inst/settings/classComparisonsWithJson.csv")
 
-permutations$sql <- allCohortsSql
+# permutations$sql <- allCohortsSql
 permutations <- permutations %>%
   mutate(atlasName = paste0(shortName, " CVD: ", cvd, " Age: ", age)) %>%
-  mutate(name = paste0("ID", cohortId))
+  mutate(name = paste0("ID", as.integer(cohortId)))
 
 for (i in 1:nrow(permutations)) {
   row <- permutations[i,]
-  sqlFileName <- file.path("inst/sql/sql_server", paste(row$name, "sql", sep = "."))
-  SqlRender::writeSql(row$sql, sqlFileName)
-  jsonFileName <- file.path("inst/cohorts", paste(row$name, "json", sep = "."))
+  # sqlFileName <- file.path("inst/sql/sql_server/class", paste(row$name, "sql", sep = "."))
+  # SqlRender::writeSql(row$sql, sqlFileName)
+  jsonFileName <- file.path("inst/cohorts/class", paste(row$name, "json", sep = "."))
   SqlRender::writeSql(row$json, jsonFileName)
 }
 
-cohortsToCreate <- permutations %>% mutate(atlasId = cohortId) %>%
-  select(atlasId, atlasName, cohortId, name)
+# Generate ingredient-level cohorts
+permutations <- readr::read_csv("inst/settings/classComparisons.csv")
+exposuresOfInterestTable <- readr::read_csv("inst/settings/ExposuresOfInterest.csv")
+permutations <- inner_join(permutations, exposuresOfInterestTable %>% select(cohortId, shortName), by = c("targetId" = "cohortId"))
 
-readr::write_csv(cohortsToCreate, file.path("inst/settings", "CohortsToCreate.csv"))
+permutations <- permutations %>% filter(cohortId == 101100000)
 
-comparisons <- readr::read_csv("inst/settings/classComparisonsWithJson.csv")
+classId <- 10
+drugsForClass <- exposuresOfInterestTable %>% filter(cohortId > classId, cohortId < (classId + 10)) %>% mutate(classId = classId)
+permutationsForDrugs <- drugsForClass %>% left_join(permutations, by = c("classId" ="targetId")) %>%
+  mutate(targetId = cohortId.x,
+         cohortId = cohortId.y,
+         includedConceptIds = conceptId,
+         shortName = name) %>%
+  select(-type, -shortName.x, -order, -includedConceptIds, -conceptId, -cohortId.x, -cohortId.y, -shortName.y) %>%
+  rowwise() %>%
+  mutate(cohortId = as.integer(sub(paste0("^",classId), targetId, cohortId))) %>%
+  mutate(name = paste0("ID", as.integer(cohortId)))
+
+permutationsForDrugs$json <-
+  do.call("rbind",
+          lapply(1:nrow(permutationsForDrugs), function(i) {
+            cohortDefinition <- permuteTC(baseCohort, permutationsForDrugs[i,], ingredientLevel = TRUE)
+            cohortJson <- RJSONIO::toJSON(cohortDefinition$expression, indent = 2, digits = 10)
+            return(cohortJson)
+            #return(cohortDefinition)
+          }))
+
+for (i in 1:nrow(permutationsForDrugs)) {
+  row <- permutationsForDrugs[i,]
+  # sqlFileName <- file.path("inst/sql/sql_server/class", paste(row$name, "sql", sep = "."))
+  # SqlRender::writeSql(row$sql, sqlFileName)
+  jsonFileName <- file.path("inst/cohorts/drug", paste(row$name, "json", sep = "."))
+  SqlRender::writeSql(row$json, jsonFileName)
+}
+
+# cohortsToCreate <- permutations %>% mutate(atlasId = cohortId) %>%
+#   select(atlasId, atlasName, cohortId, name)
+#
+# readr::write_csv(cohortsToCreate, file.path("inst/settings", "CohortsToCreate.csv"))
+
+#comparisons <- readr::read_csv("inst/settings/classComparisonsWithJson.csv")
 
 
 
