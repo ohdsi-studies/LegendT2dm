4d4325294a45455ffc721ee7ff359104d4ffa370
Marc Suchard <msuchard@ucla.edu>
2021-06-23 15:48:46 -0700

    back-end database appears to work well now

--- inst/sql/postgresql/CreatePsAssessmentTables.sql
+++ inst/sql/postgresql/CreatePsAssessmentTables.sql
@@ -1,22 +1,28 @@
+-- Drop old tables if exists
 
 DROP TABLE IF EXISTS ps_auc_assessment;
 DROP TABLE IF EXISTS ps_covariate_assessment;
 
+-- Create tables
+
 CREATE TABLE ps_auc_assessment (
-			target_id BIGINT NOT NULL,
-			comparator_id BIGINT NOT NULL,
-			database_id VARCHAR NOT NULL,
-			auc FLOAT,
-			equipoise FLOAT,
-			PRIMARY KEY(database_id, target_id, comparator_id)
+     database_id VARCHAR(255) NOT NULL,
+     target_id BIGINT NOT NULL,
+     comparator_id BIGINT NOT NULL,
+     auc NUMERIC NOT NULL,
+     equipoise NUMERIC NOT NULL,
+     comparison TEXT ,
+     PRIMARY KEY(database_id, target_id, comparator_id)
 );
 
 CREATE TABLE ps_covariate_assessment (
-			target_id BIGINT NOT NULL,
-			comparator_id BIGINT NOT NULL,
-			database_id VARCHAR NOT NULL,
-			coefficient FLOAT,
-			covariate_id BIGINT NOT NULL,
-			covariate_name VARCHAR,
-			PRIMARY KEY(database_id, target_id, comparator_id)
+     target_id BIGINT NOT NULL,
+     comparator_id BIGINT NOT NULL,
+     database_id VARCHAR(255) NOT NULL,
+     covariate_id BIGINT NOT NULL,
+     comparison TEXT ,
+     covariate_name TEXT ,
+     coefficient NUMERIC NOT NULL,
+     PRIMARY KEY(target_id, comparator_id, database_id, covariate_id)
 );
+
