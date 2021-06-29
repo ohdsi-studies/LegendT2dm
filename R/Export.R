455610d5c7fa6015850bdfff88fea649f3e138f7
Marc A. Suchard <msuchard@ucla.edu>
2021-06-28 18:06:43 -0700

    add databaseId to ps_auc_assessment

--- R/Export.R
+++ R/Export.R
@@ -960,7 +960,8 @@ exportDiagnostics <- function(indicationId,
                                           ps$preferenceScore <= 0.7),
                                   targetId = row$targetId,
                                   comparatorId = row$comparatorId,
-                                  analysisId = row$analysisId)
+                                  analysisId = row$analysisId,
+                                  databaseId = databaseId)
             return(auc)
         }
         return(NULL)
