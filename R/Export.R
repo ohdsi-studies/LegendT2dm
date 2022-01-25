6e51c2f9e976cd6797d9e8da42fd6e6636d83b3e
Marc Suchard <msuchard@ucla.edu>
2022-01-25 09:49:00 -0800

    fix export of analysis lists

--- R/Export.R
+++ R/Export.R
@@ -153,13 +153,11 @@ exportAnalyses <- function(indicationId, outputFolder, exportFolder, databaseId)
 
     tempFileName <- tempfile()
 
-    ot1CmAnalysisListFile <- system.file("settings", "ot1CmAnalysisList.json", package = "LegendT2dm")
-    ittCmAnalysisListFile <- system.file("settings", "ittCmAnalysisList.json", package = "LegendT2dm")
-    ot2CmAnalysisListFile <- system.file("settings", "ot2CmAnalysisList.json", package = "LegendT2dm")
-
-    ot1CmAnalysisList <- CohortMethod::loadCmAnalysisList(ot1CmAnalysisListFile)
-    ittCmAnalysisList <- CohortMethod::loadCmAnalysisList(ittCmAnalysisListFile)
-    # ot2CmAnalysisList <- CohortMethod::loadCmAnalysisList(ot2CmAnalysisListFile)
+    loadList <- function(fileName) {
+      CohortMethod::loadCmAnalysisList(system.file("settings",
+                                                   fileName,
+                                                   package = "LegendT2dm"))
+    }
 
     cmAnalysisToRow <- function(cmAnalysis) {
         ParallelLogger::saveSettingsToJson(cmAnalysis, tempFileName)
@@ -169,9 +167,12 @@ exportAnalyses <- function(indicationId, outputFolder, exportFolder, databaseId)
         return(row)
     }
 
-    cmAnalysisList <- c(ot1CmAnalysisList, ittCmAnalysisList
-                        # , ot2CmAnalysisList
-                            )
+    cmAnalysisList <- c(loadList("ot1CmAnalysisList.json"),
+                        loadList("ittCmAnalysisList.json"),
+                        loadList("ot2CmAnalysisList.json"),
+                        loadList("ot1PoCmAnalysisList.json"),
+                        loadList("ittPoCmAnalysisList.json"),
+                        loadList("ot2PoCmAnalysisList.json"))
 
     cohortMethodAnalysis <- lapply(cmAnalysisList, cmAnalysisToRow)
     cohortMethodAnalysis <- do.call("rbind", cohortMethodAnalysis)
