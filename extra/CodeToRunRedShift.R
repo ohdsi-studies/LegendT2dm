e5431ef62236823d3e7ecfebadc1ba44661e7a3b
Admin_FBu2 <Admin_FBu2@its.jnj.com>
2023-10-30 22:17:21 -0400

    EC2 local run save

--- extra/CodeToRunRedShift.R
+++ extra/CodeToRunRedShift.R
@@ -8,24 +8,24 @@ options(andromedaTempFolder = "E:/andromedaTemp")
 oracleTempSchema <- NULL
 
 # # Feb 2023: fast forward data version to the latest accessible
-cdmDatabaseSchema <- "cdm_truven_ccae_v2182"
-serverSuffix <- "truven_ccae"
-cohortDatabaseSchema <- "scratch_fbu2"
-databaseId <- "CCAE"
-databaseName <- "IBM Health MarketScan Commercial Claims and Encounters Database"
-databaseDescription <- "IBM Health MarketScan® Commercial Claims and Encounters Database (CCAE) represent data from individuals enrolled in United States employer-sponsored insurance health plans. The data includes adjudicated health insurance claims (e.g. inpatient, outpatient, and outpatient pharmacy) as well as enrollment data from large employers and health plans who provide private healthcare coverage to employees, their spouses, and dependents. Additionally, it captures laboratory tests for a subset of the covered lives. This administrative claims database includes a variety of fee-for-service, preferred provider organizations, and capitated health plans."
-tablePrefix <- "legend_t2dm_ccae"
-outputFolder <- "E:/LegendT2dmOutput_ccae_drug2"
+# cdmDatabaseSchema <- "cdm_truven_ccae_v2182"
+# serverSuffix <- "truven_ccae"
+# cohortDatabaseSchema <- "scratch_fbu2"
+# databaseId <- "CCAE"
+# databaseName <- "IBM Health MarketScan Commercial Claims and Encounters Database"
+# databaseDescription <- "IBM Health MarketScan® Commercial Claims and Encounters Database (CCAE) represent data from individuals enrolled in United States employer-sponsored insurance health plans. The data includes adjudicated health insurance claims (e.g. inpatient, outpatient, and outpatient pharmacy) as well as enrollment data from large employers and health plans who provide private healthcare coverage to employees, their spouses, and dependents. Additionally, it captures laboratory tests for a subset of the covered lives. This administrative claims database includes a variety of fee-for-service, preferred provider organizations, and capitated health plans."
+# tablePrefix <- "legend_t2dm_ccae"
+# outputFolder <- "E:/LegendT2dmOutput_ccae_drug2"
 
 # # Feb 2023: fast forward data version to the latest accessible
-# cdmDatabaseSchema <- "cdm_optum_ehr_v2247" #v2137
-# serverSuffix <- "optum_ehr"
-# cohortDatabaseSchema <- "scratch_fbu2"
-# databaseId <- "OptumEHR"
-# databaseName <- "Optum© de-identified Electronic Health Record Dataset"
-# databaseDescription <- "Optum© de-identified Electronic Health Record Dataset represents Humedica’s Electronic Health Record data a medical records database. The medical record data includes clinical information, inclusive of prescriptions as prescribed and administered, lab results, vital signs, body measurements, diagnoses, procedures, and information derived from clinical Notes using Natural Language Processing (NLP)."
-# tablePrefix <- "legend_t2dm_optum_ehr"
-# outputFolder <- "E:/LegendT2dmOutput_optum_ehr_drug"
+cdmDatabaseSchema <- "cdm_optum_ehr_v2247" #v2137
+serverSuffix <- "optum_ehr"
+cohortDatabaseSchema <- "scratch_fbu2"
+databaseId <- "OptumEHR"
+databaseName <- "Optum© de-identified Electronic Health Record Dataset"
+databaseDescription <- "Optum© de-identified Electronic Health Record Dataset represents Humedica’s Electronic Health Record data a medical records database. The medical record data includes clinical information, inclusive of prescriptions as prescribed and administered, lab results, vital signs, body measurements, diagnoses, procedures, and information derived from clinical Notes using Natural Language Processing (NLP)."
+tablePrefix <- "legend_t2dm_optum_ehr"
+outputFolder <- "E:/LegendT2dmOutput_optum_ehr_drug2"
 
 # Feb 2023: fast forward data version to the latest accessible
 # cdmDatabaseSchema <- "cdm_truven_mdcr_v2322" #v2183
@@ -35,7 +35,7 @@ outputFolder <- "E:/LegendT2dmOutput_ccae_drug2"
 # databaseName <- "IBM Health MarketScan Medicare Supplemental and Coordination of Benefits Database"
 # databaseDescription <- "IBM Health MarketScan® Medicare Supplemental and Coordination of Benefits Database (MDCR) represents health services of retirees in the United States with primary or Medicare supplemental coverage through privately insured fee-for-service, point-of-service, or capitated health plans. These data include adjudicated health insurance claims (e.g. inpatient, outpatient, and outpatient pharmacy). Additionally, it captures laboratory tests for a subset of the covered lives."
 # tablePrefix <- "legend_t2dm_mdcr"
-# outputFolder <- "E:/LegendT2dmOutput_mdcr_continuousAge_test"
+# outputFolder <- "E:/LegendT2dmOutput_mdcr_drug2"
 
 # # Feb 2023: fast forward data version to the latest accessible
 # TBD: run drug-level study on MDCD
@@ -49,14 +49,14 @@ outputFolder <- "E:/LegendT2dmOutput_ccae_drug2"
 # outputFolder <- "E:/LegendT2dmOutput_mdcd_drug2"
 
 # # Feb 2023: fast forward data version to the latest accessible
-# cdmDatabaseSchema <- "cdm_optum_extended_dod_v2323" #v2228 #v2134
-# serverSuffix <- "optum_extended_dod"
-# cohortDatabaseSchema <- "scratch_fbu2"
-# databaseId <- "OptumDod"
-# databaseName <- "Optum Clinformatics Extended Data Mart - Date of Death (DOD)"
-# databaseDescription <- "Optum Clinformatics Extended DataMart is an adjudicated US administrative health claims database for members of private health insurance, who are fully insured in commercial plans or in administrative services only (ASOs), Legacy Medicare Choice Lives (prior to January 2006), and Medicare Advantage (Medicare Advantage Prescription Drug coverage starting January 2006).  The population is primarily representative of commercial claims patients (0-65 years old) with some Medicare (65+ years old) however ages are capped at 90 years.  It includes data captured from administrative claims processed from inpatient and outpatient medical services and prescriptions as dispensed, as well as results for outpatient lab tests processed by large national lab vendors who participate in data exchange with Optum.  This dataset also provides date of death (month and year only) for members with both medical and pharmacy coverage from the Social Security Death Master File (however after 2011 reporting frequency changed due to changes in reporting requirements) and location information for patients is at the US state level."
-# tablePrefix <- "legend_t2dm_optum_dod"
-# outputFolder <- "E:/LegendT2dmOutput_optum_dod_drug2"
+cdmDatabaseSchema <- "cdm_optum_extended_dod_v2323" #v2228 #v2134
+serverSuffix <- "optum_extended_dod"
+cohortDatabaseSchema <- "scratch_fbu2"
+databaseId <- "OptumDod"
+databaseName <- "Optum Clinformatics Extended Data Mart - Date of Death (DOD)"
+databaseDescription <- "Optum Clinformatics Extended DataMart is an adjudicated US administrative health claims database for members of private health insurance, who are fully insured in commercial plans or in administrative services only (ASOs), Legacy Medicare Choice Lives (prior to January 2006), and Medicare Advantage (Medicare Advantage Prescription Drug coverage starting January 2006).  The population is primarily representative of commercial claims patients (0-65 years old) with some Medicare (65+ years old) however ages are capped at 90 years.  It includes data captured from administrative claims processed from inpatient and outpatient medical services and prescriptions as dispensed, as well as results for outpatient lab tests processed by large national lab vendors who participate in data exchange with Optum.  This dataset also provides date of death (month and year only) for members with both medical and pharmacy coverage from the Social Security Death Master File (however after 2011 reporting frequency changed due to changes in reporting requirements) and location information for patients is at the US state level."
+tablePrefix <- "legend_t2dm_optum_dod"
+outputFolder <- "E:/LegendT2dmOutput_optum_dod_drug2"
 
 
 conn <- DatabaseConnector::createConnectionDetails(
@@ -88,15 +88,15 @@ assessPhenotypes(connectionDetails = conn,
                  createOutcomeCohorts = TRUE,
                  runOutcomeCohortDiagnostics = FALSE) #TRUE)
 
-# assessPropensityModels(connectionDetails = conn,
-#                        cdmDatabaseSchema = cdmDatabaseSchema,
-#                        tablePrefix = tablePrefix,
-#                        indicationId = 'drug',
-#                        oracleTempSchema = oracleTempSchema,
-#                        cohortDatabaseSchema = cohortDatabaseSchema,
-#                        outputFolder = outputFolder,
-#                        databaseId = databaseId,
-#                        maxCores = 16)
+assessPropensityModels(connectionDetails = conn,
+                       cdmDatabaseSchema = cdmDatabaseSchema,
+                       tablePrefix = tablePrefix,
+                       indicationId = 'drug',
+                       oracleTempSchema = oracleTempSchema,
+                       cohortDatabaseSchema = cohortDatabaseSchema,
+                       outputFolder = outputFolder,
+                       databaseId = databaseId,
+                       maxCores = 16)
 
 
 # Cohort Explorer
@@ -157,27 +157,28 @@ execute(connectionDetails = conn,
         runSections = c(1:6),
         computeCovariateBalance = TRUE,
         exportToCsv = TRUE,
-        maxCores = 16)
+        maxCores = 10)
 
 # quick bug-fix to re-package results (likelihood profile re-extraction)
 # re-run SGLT2i with OT2 only
-# execute(connectionDetails = conn,
-#         cdmDatabaseSchema = cdmDatabaseSchema,
-#         oracleTempSchema = oracleTempSchema,
-#         cohortDatabaseSchema = cohortDatabaseSchema,
-#         outputFolder = outputFolder,
-#         indicationId = "sglt2i",
-#         databaseId = databaseId,
-#         databaseName = databaseName,
-#         databaseDescription = databaseDescription,
-#         tablePrefix = tablePrefix,
-#         createExposureCohorts = FALSE,
-#         createOutcomeCohorts = TRUE,
-#         fetchAllDataFromServer = TRUE,
-#         generateAllCohortMethodDataObjects = TRUE,
-#         runCohortMethod = TRUE,
-#         runSections = c(3,6),
-#         computeCovariateBalance = TRUE,
-#         exportToCsv = TRUE,
-#         maxCores = 16)
-#
+# try fixing balance & export issue for OptumEHR all drugs
+# try re-packaging OptumEHR result files
+execute(connectionDetails = conn,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        oracleTempSchema = oracleTempSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        outputFolder = outputFolder,
+        indicationId = "drug",
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        tablePrefix = tablePrefix,
+        createExposureCohorts = FALSE,
+        createOutcomeCohorts = FALSE,
+        fetchAllDataFromServer = FALSE,
+        generateAllCohortMethodDataObjects = FALSE,
+        runCohortMethod = FALSE,
+        runSections = c(1:6),
+        computeCovariateBalance = FALSE,
+        exportToCsv = TRUE,
+        maxCores = 4)
