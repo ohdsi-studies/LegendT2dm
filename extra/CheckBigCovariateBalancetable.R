14bcf0c4e39cd064e5ddcbd338e0662182fee3d0
Fan Bu <fanbu@Fans-MacBook-Pro-2.local>
2024-05-15 23:44:08 -0400

    add script to check OptumEHR balance table

---
+++ extra/CheckBigCovariateBalancetable.R
@@ -0,0 +1,39 @@
+# helper functions to check over OptumEHR covariate balance table before uploading again
+
+library(dplyr)
+
+checkBalance <- function(chunk, pos){
+  printf("Checking row %s through %s ...\n",
+         pos, nrow(chunk)+pos-1)
+
+  sel <- chunk %>%
+    filter(database_id == "OptumEHR",
+           target_id == 311100000,
+           comparator_id == 331100000,
+           analysis_id %in% c(5,6))
+
+  if(nrow(sel) > 0){
+    cat("Found it!! \n\n")
+    maxStd <- sel %>% group_by(analysis_id) %>%
+      summarize(maxStdDiff = max(std_diff_after, na.rm = TRUE)) %>%
+      ungroup() %>%
+      pull(maxStdDiff)
+
+    cat("The maxStdDiff after is:",
+        paste(round(maxStd,4), collapse = ", "),
+        "\n\n\n")
+  }
+}
+
+
+thePath = ""
+csvFileName = "covariate_balance.csv"
+
+readr::read_csv_chunked(
+  file = file.path(thePath, csvFileName),
+  callback = uploadChunk,
+  chunk_size = 1e6,
+  col_types = readr::cols(),
+  guess_max = 1e5,
+  progress = FALSE
+)
